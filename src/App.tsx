0a261f98f1bfc46fce09a496aa2e5fdddf3dc55e
jalxby <jalxby@gmail.com>
2023-01-22 21:20:10 +0100

    commit - 02

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,16 @@ import './App.css';
 import {TodoList} from "./TodoList";
 
 function App() {
-    let task1 = [
-        {id: 1, title:"CSS", isDone: true},
-        {id: 2, title:"JS", isDone: true},
-        {id: 3, title:"REACT", isDone: true}
+    let task = [
+        {id: 1, title: "CSS", isDone: true},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "REACT", isDone: false},
+        {id: 4, title: "NODE.JS", isDone: false}
     ]
-    let task2 = [
-        {id: 1, title:"Terminator", isDone: true},
-        {id: 2, title:"XXX", isDone: false},
-        {id: 3, title:"Jentelmens of fortune", isDone: true}
-    ]
+
     return (
         <div className="App">
-            <TodoList title={"What to learn"}tasks={task1}/>
-            <TodoList title={"Movies"} tasks={task2}/>
+            <TodoList title={"What to learn"} tasks={task}/>
 
         </div>
     );
